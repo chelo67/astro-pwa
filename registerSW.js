b30046bce58cf673663ade3facd3bab585f14203
marcelo <marceloacampora@gmail.com>
2024-09-02 14:09:39 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/https://reposocratech.github.io/PWA-Youtube/sw.js', { scope: '/https://reposocratech.github.io/PWA-Youtube/' })})}
