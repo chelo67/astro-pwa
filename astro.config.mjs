3a1cfcad32d7a9144b1f43f4d73631db04b2b57a
marcelo <marceloacampora@gmail.com>
2024-09-02 14:05:18 -0300

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,41 @@
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://chelo67.github.io',
     base: 'astro-pwa',
-  })
+    integrations: [AstroPWA({
+      manifest: {
+				display: 'standalone',
+				display_override: ['window-controls-overlay'],
+				lang: 'es-ES',
+				name: 'Astro Pwa',
+				short_name: 'Astro Pwa 08',
+				description: 'Ejemplo de PWA creada en Socratech',
+				theme_color: '#19223c',
+				background_color: '#d4d4d4',
+				icons: [
+					{
+						src: 'icon-72x72.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'apple-touch-icon.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		}),
+	],
+	base: 'https://reposocratech.github.io/PWA-Youtube/',
+})
